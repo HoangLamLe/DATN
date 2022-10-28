fa218f9615f94388fb5b8e26a102677d1521cd2f
HoangLamLe <meoulam192@gmail.com>
2022-10-28 14:58:10 +0700

    Update UI for temp and humidity chart

--- src/components/charts/GaugeChart.jsx
+++ src/components/charts/GaugeChart.jsx
@@ -1,4 +1,3 @@
-import GaugeChart from "react-gauge-chart";
 import ReactSpeedometer from "react-d3-speedometer";
 
 import "./GaugeChart.scss";
@@ -7,16 +6,16 @@ export function Power(props) {
   return (
     <div className="gauge-container">
       <ReactSpeedometer
-        width={500}
-        height={500}
+        width={400}
+        height={250}
         minValue={-50}
         maxValue={120}
-        value={50}
+        value={Math.round(Math.random() * 120)}
         customSegmentStops={[-50, 0, 20, 45, 100, 120]}
         segmentColors={["#AFEEEE", "blue", "#00ff00", "yellow", "red"]}
         needleColor="rgb(19 6 84 / 40%)"
         needleHeightRatio={0.9}
-        currentValueText={'Nhiệt độ : ${value} °C'}
+        currentValueText={"Nhiệt độ : ${value} °C"}
         paddingVertical={15}
         forceRender={true}
       />
--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
-import { Select, Button } from "antd";
+import { Select, Button, Modal } from "antd";
 
 import "./Display.scss";
 import { Power } from "../charts/GaugeChart";
+import BarChart from "../charts/BarChart";
+import HumidBarChart from "../charts/HumidBarChart";
 
 const { Option } = Select;
 
 function Display() {
   const [keyValue, setKeyValue] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (value) => {
     setKeyValue(value);
   };
 
+  const handleCloseModal = () => {
+    handleChange("");
+    setIsSubmit(false);
+  };
+
   const handleSubmitButton = () => {
-    console.log("Data submit", keyValue);
+    if (!keyValue) return;
+    setIsSubmit(true);
   };
   return (
     <div className="container">
       <Select
+        value={keyValue}
         size="large"
         placeholder="Chọn dữ liệu muốn hiển thị"
         onSelect={handleChange}
@@ -32,12 +42,39 @@ function Display() {
         {keyValue ? keyValue : "Chưa chọn key value"}
       </div>
       <div className="submit-button">
-        <Button onClick={() => handleSubmitButton()} type="primary">
+        <Button
+          onClick={() => handleSubmitButton()}
+          type="primary"
+          disabled={!keyValue}
+        >
           Xem
         </Button>
       </div>
       <div className="charts">
-        <Power />
+        <Modal
+          open={keyValue === "Nhiệt độ" && isSubmit}
+          onCancel={() => handleCloseModal()}
+          onOk={() => handleCloseModal()}
+          width="50vw"
+        >
+          <Power />
+        </Modal>
+        <Modal
+          open={keyValue === "Độ ẩm" && isSubmit}
+          onCancel={() => handleCloseModal()}
+          onOk={() => handleCloseModal()}
+          width="50vw"
+        >
+          <HumidBarChart />
+        </Modal>
+        <Modal
+          open={keyValue === "Lượng mưa" && isSubmit}
+          onCancel={() => handleCloseModal()}
+          onOk={() => handleCloseModal()}
+          width="50vw"
+        >
+          <BarChart />
+        </Modal>
       </div>
     </div>
   );
